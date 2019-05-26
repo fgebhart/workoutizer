520b874f5eb1285f34453d4d6883363cf0cce6a9
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-05-26 11:44:58 +0200

    organize navbar - make responsive

--- wizer/dashboard/static/js/script.js
+++ wizer/dashboard/static/js/script.js
@@ -3,3 +3,21 @@
 $(document).ready(function () {
     $(".dropdown-trigger").dropdown();
 });
+
+
+// enable sidenav
+document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
