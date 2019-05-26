6c20b41f3b6ca44f44dbeb2660de2ea3bc01beb6
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-05-26 13:08:15 +0200

    add new pages

--- wizer/dashboard/urls.py
+++ wizer/dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from dashboard.views import DashboardView, ActivityView
+from dashboard.views import DashboardView, ActivityView, AddActivityView, SportsView, AddSportsView
 
 
 urlpatterns = [
     path('', DashboardView.as_view(), name="dashboard.html"),
-    path('activity', ActivityView.as_view(), name="activity.html"),
+    path('activity', ActivityView.as_view(), name="activity/activity.html"),
+    path('activity/add', AddActivityView.as_view(), name="activity/add_activity.html"),
+    path('sports', SportsView.as_view(), name="sports/sports.html"),
+    path('sports/add', AddSportsView.as_view(), name="sports/add_sports.html"),
 ]
--- wizer/dashboard/views.py
+++ wizer/dashboard/views.py
@@ -4,12 +4,34 @@ from django.views.generic import View
 
 class DashboardView(View):
     template_name = "dashboard.html"
+
     def get(self, request):
         return render(request, self.template_name)
 
 
-
 class ActivityView(View):
-    template_name = "activity.html"
+    template_name = "activity/activity.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class AddActivityView(View):
+    template_name = "activity/add_activity.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class SportsView(View):
+    template_name = "sports/sports.html"
+
+    def get(self, request):
+        return render(request, self.template_name)
+
+
+class AddSportsView(View):
+    template_name = "sports/add_sports.html"
+
     def get(self, request):
         return render(request, self.template_name)
