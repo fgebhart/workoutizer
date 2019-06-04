68e91d76acefd9da212600295d95500a4c9b01ce
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-06-04 22:37:50 +0200

    enable to check for new files in trace dir in a parallel process

--- workoutizer/wizer/apps.py
+++ workoutizer/wizer/apps.py
@@ -1,7 +1,9 @@
+import time
+import os
 import logging
+from multiprocessing import Process
 
 from django.apps import AppConfig
-
 
 log = logging.getLogger('wizer')
 
@@ -12,11 +14,23 @@ class WizerConfig(AppConfig):
 
     def ready(self):
         log.debug("apps successfully loaded")
+        from .models import Settings
+        settings = Settings.objects.get(id=1)
+        p = Process(target=FileChecker, args=(settings.path_to_trace_dir,))
+        p.start()
+        log.warning("sent to background - starting django")
 
 
 class FileChecker:
+    def __init__(self, path):
+        self.path = path
+        log.debug(f"going to listen for new files in dir: {path}")
+        self.start_listening()
 
-    def __init__(self):
-        from .models import Settings
-        settings = Settings.objects.get(id=1)
-        print(f"path to trace dir: {settings.path_to_trace_dir}")
+    def start_listening(self):
+        while True:
+            files_in_dir = os.listdir(path=self.path)
+            log.debug(f"found files: {files_in_dir}")
+            time.sleep(3)
+
+
