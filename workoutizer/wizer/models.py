2bff8ae0d7b1fdc9ecca690f034dc92243ac4583
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-08-28 19:16:29 +0200

    delete also trace file db entry when activity is deleted

--- workoutizer/wizer/models.py
+++ workoutizer/wizer/models.py
@@ -57,7 +57,7 @@ class Activity(models.Model):
     duration = models.FloatField()
     distance = models.FloatField(blank=True, null=True)
     description = models.CharField(max_length=300, blank=True, null=True)
-    trace_file = models.ForeignKey(TraceFiles, on_delete=models.SET_NULL, blank=True, null=True)
+    trace_file = models.ForeignKey(TraceFiles, on_delete=models.CASCADE, blank=True, null=True)
 
 
 class Settings(models.Model):
