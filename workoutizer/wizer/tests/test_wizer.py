52e83dd1c60e7888f96898fca1f88ccf5842fdce
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-08-28 19:15:53 +0200

    "other" sport exists no longer, sport will be None when

--- workoutizer/wizer/tests/test_wizer.py
+++ workoutizer/wizer/tests/test_wizer.py
@@ -23,4 +23,3 @@ sport_map = {
 def test_map_sport_name():
     assert map_sport_name('running', sport_map) == "Jogging"
     assert map_sport_name('Running', sport_map) == "Jogging"
-    assert map_sport_name('something else', sport_map) == "other"
