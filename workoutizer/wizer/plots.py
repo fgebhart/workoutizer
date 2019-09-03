68ecbaee7835a91812d4745dc9b25e3e3ece3149
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-09-03 22:41:14 +0200

    add hover tool tip, fix #65

--- workoutizer/wizer/plots.py
+++ workoutizer/wizer/plots.py
@@ -2,7 +2,7 @@ import logging
 import datetime
 
 import pandas as pd
-from bokeh.models import ColumnDataSource
+from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import figure
 
 from django.conf import settings
@@ -54,8 +54,19 @@ def plot_activities(activities):
         'colors': colors,
     }
 
-    p.multi_line(xs='xs', ys='ys', color='colors', line_width=3, legend='legend', source=ColumnDataSource(data))
+    p.multi_line(xs='xs', ys='ys', color='colors', line_width=4, legend='legend', hover_line_color='colors',
+                 hover_line_alpha=1.0, source=ColumnDataSource(data))
     p.legend.label_text_font = "Ubuntu"
     p.legend.location = "top_left"
+
+    # TODO: add custom tool tip with sport icon: https://bokeh.pydata.org/en/latest/docs/user_guide/tools.html#custom-tooltip
+    p.add_tools(HoverTool(
+        show_arrow=True,
+        line_policy='next',
+        tooltips=[
+            ('Sport', '@legend'),
+            ('Duration', '$y'),
+        ],
+    ))
 
     return p
