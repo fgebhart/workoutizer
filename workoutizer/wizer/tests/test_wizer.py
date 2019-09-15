234fc669b168e89c37a71f4da3531fbdd74704cd
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-09-15 08:02:07 +0200

    fix pipeline, for real

--- workoutizer/wizer/tests/test_wizer.py
+++ workoutizer/wizer/tests/test_wizer.py
@@ -1,5 +1,5 @@
 from workoutizer.wizer.tools.utils import sanitize
-from wizer.gis.gpx_converter import calc_distance_of_points
+from workoutizer.wizer.gis.gpx_converter import calc_distance_of_points
 from workoutizer.wizer.apps import map_sport_name
 
 
