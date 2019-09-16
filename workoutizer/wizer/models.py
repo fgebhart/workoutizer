915e1e42d301a63119363e01a5683516a45084be
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-09-16 11:43:26 +0200

    add verbose name to model for form label

--- workoutizer/wizer/models.py
+++ workoutizer/wizer/models.py
@@ -14,10 +14,10 @@ class Sport(models.Model):
     def __str__(self):
         return self.name
 
-    name = models.CharField(max_length=24, unique=True)
+    name = models.CharField(max_length=24, unique=True, verbose_name="Sport Name:")
     slug = models.CharField(max_length=24, unique=True, editable=False)
-    color = models.CharField(max_length=24, unique=True)
-    icon = models.CharField(max_length=24)
+    color = models.CharField(max_length=24, unique=True, verbose_name="Color:")
+    icon = models.CharField(max_length=24, verbose_name="Icon:")
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
@@ -71,12 +71,12 @@ class Activity(models.Model):
     def __str__(self):
         return f"{self.title} ({self.sport})"
 
-    title = models.CharField(max_length=50)
-    sport = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True)
-    date = models.DateField(blank=False, default=timezone.now)
-    duration = models.IntegerField()
-    distance = models.FloatField(blank=True, null=True)
-    description = models.CharField(max_length=300, blank=True, null=True)
+    title = models.CharField(max_length=50, verbose_name="Activity Name:")
+    sport = models.ForeignKey(Sport, on_delete=models.SET_NULL, null=True, verbose_name="Sport:")
+    date = models.DateField(blank=False, default=timezone.now, verbose_name="Date:")
+    duration = models.IntegerField(verbose_name="Duration:")
+    distance = models.FloatField(blank=True, null=True, verbose_name="Distance:")
+    description = models.CharField(max_length=300, blank=True, null=True, verbose_name="Description:")
     trace_file = models.ForeignKey(Traces, on_delete=models.CASCADE, blank=True, null=True)
 
 
@@ -84,6 +84,8 @@ class Settings(models.Model):
     days_choices = [(365, 365), (180, 180), (30, 30), (10, 10), (5, 5)]
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=settings.AUTH_USER_MODEL)
-    path_to_trace_dir = models.CharField(max_length=120)
-    gpx_checker_interval = models.IntegerField()
+    path_to_trace_dir = models.CharField(max_length=120, verbose_name="Path to GPX Files Directory:")
+    gpx_checker_interval = models.IntegerField(verbose_name="GPX File Checker Time Interval:")
     number_of_days = models.IntegerField(choices=days_choices, default=30)
+    trace_width = models.FloatField(max_length=20, default=3.0, verbose_name="Width of Trace:")
+    trace_opacity = models.FloatField(max_length=20, default=0.7, verbose_name="Opacity of Trace:")
