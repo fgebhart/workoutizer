842dc4497a869a0d26400cb31a1e4aa616a1501d
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-09-23 10:28:07 +0200

    debugging ci

--- workoutizer/workoutizer/settings.py
+++ workoutizer/workoutizer/settings.py
@@ -17,11 +17,8 @@ import coloredlogs
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Project Directory (where settings.py is located)
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
-
 # Fixtures directory
-FIXTURE_DIRS = (os.path.join(PROJECT_DIR, 'fixtures'),)
+FIXTURE_DIRS = (os.path.join(BASE_DIR, 'fixtures'),)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
