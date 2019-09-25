a7d9540b208bc66cb1c0cb4ba9311b8320732d47
Fabian Gebhart <fabian.gebhart@gmail.com>
2019-09-25 10:52:56 +0200

    display duration in format HH:MM, fix #90

--- workoutizer/wizer/templatetags/filters.py
+++ workoutizer/wizer/templatetags/filters.py
@@ -10,9 +10,18 @@ register = template.Library()
 
 @register.filter
 def duration(td):
-    log.debug(f"filtering duration: {td}")
     total_seconds = int(td.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
 
     return '{}h {}m'.format(hours, minutes)
+
+
+@register.filter
+def table_duration(td):
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+    if minutes < 10:
+        minutes = f"0{minutes}"
+    return '{}:{}'.format(hours, minutes)
