ba05c2dbb9339ec05ce2ebcccf067b0da12966c2
Fabian Gebhart <fabian.gebhart@jda.com>
2019-12-15 20:02:58 +0100

    write dropbox sync scrips as functions

--- dbx/download_db.py
+++ dbx/download_db.py
@@ -4,15 +4,18 @@ import dropbox
 
 
 dbx = dropbox.Dropbox(os.environ['DROPBOXTOKEN'])
-
 file = "workoutizer/db.sqlite3"
 
-if __name__ == '__main__':
+
+def download_db():
     cwd = Path(os.path.abspath(__file__))
     parent = cwd.parent.parent
-
     print(f"downloading {file} from dropbox ...")
     with open(f"{parent}/{file}", "wb") as f:
         metadata, res = dbx.files_download(path=f"/{file}")
         f.write(res.content)
     print("done.")
+
+
+if __name__ == '__main__':
+    download_db()
