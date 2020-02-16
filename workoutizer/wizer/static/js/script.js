dd36af0ba7fe47ad42120eba921b2eb438b7b6f0
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-02-16 21:28:46 +0100

    add different color for begin (green) and end (red) leaflet marker, fix #66

---
+++ workoutizer/wizer/static/js/script.js
@@ -0,0 +1,13 @@
+var greenIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
+
+var redIcon = new L.Icon({
+    iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+});
