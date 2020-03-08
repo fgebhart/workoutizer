58a9ad763fcef7e093aa231035ab7a2a1402b776
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-03-08 22:32:13 +0100

    reformat view of activity data

--- workoutizer/wizer/templatetags/filters.py
+++ workoutizer/wizer/templatetags/filters.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 
 from django import template
@@ -13,8 +14,7 @@ def duration(td):
     total_seconds = int(td.total_seconds())
     hours = total_seconds // 3600
     minutes = (total_seconds % 3600) // 60
-
-    return '{}h {}m'.format(hours, minutes)
+    return f'{hours}h {minutes}m'
 
 
 @register.filter
@@ -24,4 +24,33 @@ def table_duration(td):
     minutes = (total_seconds % 3600) // 60
     if minutes < 10:
         minutes = f"0{minutes}"
-    return '{}:{}'.format(hours, minutes)
+    return f'{hours}:{minutes}'
+
+
+@register.filter
+def speed_to_pace(speed):
+    if speed <= 0:
+        return "00:00"
+    pace = (float(speed) * 60 / 1000) ** -1
+    delta = datetime.timedelta(minutes=pace)
+    hours, remainder = divmod(delta.seconds, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    pace = '{:02}:{:02}'.format(int(minutes), int(seconds))
+    return f'{pace}'
+
+
+@register.filter
+def m_per_s_to_km_per_h(m_per_s):
+    return round(float(m_per_s) * 3.6, 2)
+
+
+@register.filter
+def round_2nd_decimal(td):
+    return round(td, 2)
+
+
+def strfdelta(tdelta, format):
+    d = {"days": tdelta.days}
+    d["hours"], rem = divmod(tdelta.seconds, 3600)
+    d["minutes"], d["seconds"] = divmod(rem, 60)
+    return format.format(**d)
