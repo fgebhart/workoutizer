df9e2c88e135b4ec9cbaf5b1dcb6dac0dac8081c
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-04-14 21:28:14 +0200

    restructure tests folder to match structure of django app wizer

--- workoutizer/wizer/tests/file_helper/test_gpx_parser.py
+++ workoutizer/wizer/tests/file_helper/test_gpx_parser.py
@@ -18,13 +18,13 @@ def test__get_sport_duration_distance(gpx_parser):
 
 
 def test_get_date_from_metadata(gpx_parser):
-    test_file_path = os.path.join(os.path.dirname(__file__), "data/garmin_example.gpx")
+    test_file_path = os.path.join(os.path.dirname(__file__), "data/../data/garmin_example.gpx")
     parser = gpx_parser(path=test_file_path)
     assert parser.date == datetime.datetime(2019, 12, 16, 8, 58, 30, tzinfo=SimpleTZ("Z"))
 
 
 def test_get_sport_from_type(gpx_parser):
-    test_file_path = os.path.join(os.path.dirname(__file__), "data/garmin_example.gpx")
+    test_file_path = os.path.join(os.path.dirname(__file__), "data/../data/garmin_example.gpx")
     parser = gpx_parser(path=test_file_path)
     assert parser.sport == 'running'
 
