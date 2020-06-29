bc96a1f5437231f0c7c95cedee6235fbc419337d
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-06-29 10:13:09 +0200

    pass cli arguments to django's manage.py also, fix #159

--- workoutizer/__main__.py
+++ workoutizer/__main__.py
@@ -9,17 +9,21 @@ def cli():
     command_group.add_argument('run', help='runs the workoutizer application', nargs='?')
     command_group.add_argument('init', help='collects static files and migrates the db schema to a newer version',
                                nargs='?')
+    parser.add_argument("-m", "--manage", help="pass arguments to django's manage.py", nargs='+')
 
     args = parser.parse_args()
+    os.environ["DJANGO_SETTINGS_MODULE"] = "workoutizer.settings"
     if args.run == 'run':
-        os.environ["DJANGO_SETTINGS_MODULE"] = "workoutizer.settings"
         execute_from_command_line(["manage.py", "runserver"])
     elif args.run == 'init':
-        os.environ["DJANGO_SETTINGS_MODULE"] = "workoutizer.settings"
         execute_from_command_line(["manage.py", "collectstatic"])
         execute_from_command_line(["manage.py", "migrate"])
+    elif args.manage:
+        execute_from_command_line(["manage.py"] + args.manage)
     else:
+        print(f"wkz: error: unrecognized arguments")
         parser.print_help()
+        return 1
 
     return 0
 
