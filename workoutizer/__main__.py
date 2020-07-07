ce5d9261869c3eac2dd5f0b272f91d161ef2f33e
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-07-07 08:20:55 +0200

    remove unneeded function

--- workoutizer/__main__.py
+++ workoutizer/__main__.py
@@ -173,35 +173,6 @@ class ParseDict(argparse.Action):
         setattr(namespace, self.dest, d)
 
 
-def _setup_rpi_using_ansible(vendor_id: str, product_id: str, address_plus_port: str):
-    from ansible import context
-    from ansible.cli import CLI
-    from ansible.module_utils.common.collections import ImmutableDict
-    from ansible.executor.playbook_executor import PlaybookExecutor
-    from ansible.parsing.dataloader import DataLoader
-    from ansible.inventory.manager import InventoryManager
-    from ansible.vars.manager import VariableManager
-
-    loader = DataLoader()
-    context.CLIARGS = ImmutableDict(
-        tags={}, listtags=False, listtasks=False, listhosts=False, syntax=False, connection='ssh', module_path=None,
-        forks=100, remote_user='xxx', private_key_file=None, ssh_common_args=None, ssh_extra_args=None,
-        sftp_extra_args=None, scp_extra_args=None, become=True, become_method='sudo', become_user='root',
-        verbosity=True, check=False, start_at_task=None
-    )
-    inventory = InventoryManager(loader=loader, sources=())
-    variable_manager = VariableManager(loader=loader, inventory=inventory, version_info=CLI.version_info(gitinfo=False))
-    variable_manager._extra_vars = {
-        'vendor_id': vendor_id,
-        'product_id': product_id,
-        'address_plus_port': address_plus_port,
-    }
-    pbex = PlaybookExecutor(playbooks=[os.path.join(SETUP_DIR, 'setup_on_rpi.yml')], inventory=inventory,
-                            variable_manager=variable_manager,
-                            loader=loader, passwords={})
-    pbex.run()
-
-
 def _pip_install(package):
     subprocess.check_call([sys.executable, "-m", "pip", "install", package])
 
