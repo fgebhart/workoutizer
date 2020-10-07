9fc76e205cc114ab32f5e30c2c6ac50839f857d3
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-10-07 21:33:42 +0200

    parse fit data record wise to ensure same length

--- wizer/file_helper/fit_parser.py
+++ wizer/file_helper/fit_parser.py
@@ -23,71 +23,58 @@ class FITParser(Parser):
         self.fit = FitFile(self.path_to_file)
 
     def _parse_records(self):
-        coordinates = []
-        lon = None
-        lat = None
-        altitude = None
         for record in self.fit.get_messages():
-            # print(f"------------------------------ new record ------------------------------")
             record = record.get_values()
+            # parse laps
             if record.get('event') == 'lap':
                 self.laps.append(_parse_lap_data(record))
-            for label, value in record.items():
-                # print(f"{label}: {value}")
-                if label == 'sport':
-                    self.sport = value
-                if label == "total_distance":
-                    self.distance = round(float(value) / 1000, 2)
-                if label == "total_elapsed_time":
-                    self.duration = datetime.timedelta(seconds=int(value))
-                if label == "timestamp":
-                    self.date = value.replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
-                # calories
-                if label == 'total_calories':
-                    self.calories = value
-                # speed
-                if label == "enhanced_speed":
-                    self.speed_list.append(value if value else 0)
-                if label == "enhanced_avg_speed":
-                    self.avg_speed = value
-                # coordinates
-                if label == "position_long":
-                    lon = value
-                if label == "position_lat":
-                    lat = value
-                # distances
-                if label == "distance":
-                    self.distance_list.append(value)
-                # altitude
-                if label == "altitude":
-                    altitude = float(value) / 10
-                # heart rate
-                if label == "heart_rate":
-                    self.heart_rate_list.append(value)
-                if label == "avg_heart_rate":
-                    self.avg_heart_rate = value
-                # cadence
-                if label == 'cadence':
-                    self.cadence_list.append(value if value else 0)
-                if label == "avg_running_cadence":
-                    self.avg_cadence = value
-                # temperature
-                if label == "temperature":
-                    self.temperature_list.append(value)
-                if label == "avg_temperature":
-                    self.avg_temperature = value
-                # training effect
-                if label == "total_training_effect":
-                    self.aerobic_training_effect = value
-                if label == "total_anaerobic_training_effect":
-                    self.anaerobic_training_effect = value
-                # timestamps
-                if label == "timestamp":
-                    self.timestamps_list.append(value.timestamp())
-            if lat and lon:
-                coordinates.append([_to_coordinate(lon), _to_coordinate(lat)])
-                self.altitude_list.append(altitude)
-        self.coordinates_list = coordinates
+            
+            # parse list attributes
+            timestamp = record.get('timestamp')
+            self.timestamps_list.append(timestamp.timestamp() if timestamp else timestamp)
+            self.distance_list.append(record.get('distance'))
+            self.coordinates_list.append([_to_coordinate(record.get('position_long')), _to_coordinate(record.get('position_lat'))])
+            altitude = record.get('altitude')
+            self.altitude_list.append((float(altitude) / 10) if altitude else altitude)
+            self.heart_rate_list.append(record.get('heart_rate'))
+            self.temperature_list.append(record.get('temperature'))
+            self.cadence_list.append(record.get('cadence'))
+            self.speed_list.append(record.get('enhanced_speed'))
+
+            # get first value of records
+            if not self.sport:
+                self.sport = record.get("sport")
+            if not self.aerobic_training_effect:
+                self.aerobic_training_effect = record.get("total_training_effect")
+            if not self.anaerobic_training_effect:
+                self.anaerobic_training_effect = record.get("total_anaerobic_training_effect")
+            
+            # get last value of records
+            distance = record.get("total_distance")
+            if distance:
+                self.distance = round(float(distance) / 1000, 2)
+            duration = record.get("total_elapsed_time")
+            if duration:
+                self.duration = datetime.timedelta(seconds=int(duration))
+            date = record.get("timestamp")
+            if date:
+                self.date = date.replace(tzinfo=pytz.timezone(settings.TIME_ZONE))
+            calories = record.get("total_calories")
+            if calories:
+                self.calories = calories
+            avg_heart_rate = record.get("avg_heart_rate")
+            if avg_heart_rate:
+                self.avg_heart_rate = avg_heart_rate
+            avg_cadence = record.get("avg_running_cadence")
+            if avg_cadence:
+                self.avg_cadence = avg_cadence
+            avg_speed = record.get("enhanced_avg_speed")
+            if avg_speed:
+                self.avg_speed = avg_speed
+            avg_temperature = record.get("avg_temperature")
+            if avg_temperature:
+                self.avg_temperature = avg_temperature
+            
         log.debug(f"found date: {self.date}")
         log.debug(f"found number of coordinates: {len(self.coordinates_list)}")
         log.debug(f"found number of altitude: {len(self.altitude_list)}")
@@ -114,7 +101,7 @@ class FITParser(Parser):
     def set_min_max_values(self):
         attributes = self.__dict__.copy()
         for attribute, values in attributes.items():
-            if attribute.endswith("_list") and attribute != 'coordinates_list' and attribute != 'timestamps_list':
+            if attribute.endswith("_list") and attribute != 'coordinates_list' and attribute != 'timestamps_list' and attribute != 'distance_list':
                 name = attribute.replace("_list", "")
                 values = remove_nones_from_list(values)
                 if values:
@@ -124,6 +111,8 @@ class FITParser(Parser):
     def convert_list_attributes_to_json(self):
         for attribute, values in self.__dict__.items():
             if attribute.endswith("_list"):
+                print(attribute)
+                # print(values)
                 setattr(self, attribute, json.dumps(values))
 
 
