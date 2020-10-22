17c23d62b2a19e25c732c13c0fb0064b06ba5931
pa3kDaWae <perhac.patrik97@gmail.com>
2020-10-22 19:19:25 +0200

    changed display for right sidebar for smaller devices - slide out (#15)
    Co-authored-by: Patrik Perháč <patrik.perhac@student.tuke.sk>

--- wizer/static/js/script.js
+++ wizer/static/js/script.js
@@ -15,4 +15,29 @@ var redIcon = new L.Icon({
 
 $(document).ready(function () {
     $('[data-toggle="tooltip"]').tooltip();
+
+    // close right sidebar if clicked anywhere
+    $(window).click(closeSidebar);
+    // stop propagation if this element is clicked so close sidebar is not triggered
+    $('#sidebar').click(function (event) {
+        event.stopPropagation();
+    });
+    $('#open-sidebar').click(openSidebar);
 });
+
+function openSidebar(event) {
+    $("#sidebar").addClass('sidebar_opened');
+    $('#open-sidebar').hide();
+    // stop propagating so closeSidebar is not triggered right away
+    event.stopPropagation();
+}
+
+function closeSidebar() {
+    $("#sidebar").removeClass('sidebar_opened');
+    // we show the open button after the sidebar is not visible (css transition time is 0.5s)
+    if (window.innerWidth <= 992) {
+        setTimeout(function () {
+            $('#open-sidebar').show();
+        }, 500);
+    }
+}
