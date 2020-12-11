f695f2718ef70db6274ed81b874ce996f4003f8c
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-11 21:13:21 +0000

    test headless option for firefox

--- wizer/tests/browser_tests/conftest.py
+++ wizer/tests/browser_tests/conftest.py
@@ -1,9 +1,12 @@
 import pytest
 from selenium.webdriver import Firefox
+from selenium.webdriver.firefox.options import Options
 
 
 @pytest.yield_fixture(scope="session")
 def webdriver():
-    driver = Firefox()
+    options = Options()
+    options.headless = True
+    driver = Firefox(options=options)
     yield driver
     driver.quit()
