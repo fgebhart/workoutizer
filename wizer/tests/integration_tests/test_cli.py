e47fdd95f2dc0a8f17b6d13a8b984262110442b2
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-19 14:48:33 +0000

    remove flaky and slow test

--- wizer/tests/integration_tests/test_cli.py
+++ wizer/tests/integration_tests/test_cli.py
@@ -1,4 +1,4 @@
-from workoutizer.__main__ import _get_local_ip_address, _get_latest_version_of
+from workoutizer.__main__ import _get_local_ip_address
 
 
 def test__get_local_ip_address():
@@ -6,13 +6,3 @@ def test__get_local_ip_address():
     assert type(ip_address) == str
     assert len(ip_address) >= 8
     assert "." in ip_address
-
-
-def test__get_latest_version_of():
-    # should always be False since I always should have the recent one (github ci also)
-    assert _get_latest_version_of("workoutizer") is False
-
-    # should return the latest version, since it was down-pinned
-    latest_version = _get_latest_version_of("django-leaflet")
-    assert len(latest_version) > 4
-    assert "." in latest_version
