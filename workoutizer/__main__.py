028d059ed1e4d0fe93fe8e605e62232a1cebaf3c
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-19 14:29:43 +0000

    only print wkz home info message when triggered by runserver

--- workoutizer/__main__.py
+++ workoutizer/__main__.py
@@ -121,14 +121,18 @@ def _upgrade():
 
 
 def _get_latest_version_of(package: str):
-    latest_version = str(subprocess.run([sys.executable, '-m', 'pip', 'install', '{}==random'.format(package)], capture_output=True, text=True))
-    latest_version = latest_version[latest_version.find('(from versions:')+15:]
-    latest_version = latest_version[:latest_version.find(')')]
-    latest_version = latest_version.replace(' ','').split(',')[-1]
+    latest_version = str(
+        subprocess.run([sys.executable, "-m", "pip", "install", f"{package}==random"], capture_output=True, text=True)
+    )
+    latest_version = latest_version[latest_version.find("(from versions:") + 15 :]
+    latest_version = latest_version[: latest_version.find(")")]
+    latest_version = latest_version.replace(" ", "").split(",")[-1]
 
-    current_version = str(subprocess.run([sys.executable, '-m', 'pip', 'show', '{}'.format(package)], capture_output=True, text=True))
-    current_version = current_version[current_version.find('Version:')+8:]
-    current_version = current_version[:current_version.find('\\n')].replace(' ','') 
+    current_version = str(
+        subprocess.run([sys.executable, "-m", "pip", "show", f"{package}"], capture_output=True, text=True)
+    )
+    current_version = current_version[current_version.find("Version:") + 8 :]
+    current_version = current_version[: current_version.find("\\n")].replace(" ", "")
 
     return latest_version
 
