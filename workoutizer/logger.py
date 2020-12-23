f9a6f45988e2a95967f2efadde726332ba22dbca
Fabian Gebhart <fabian.gebhart@gmail.com>
2020-12-23 10:02:16 +0100

    add logging to log file for warning level (#39)
    * add logging to log file for warning level
    * ensure that wkz home dir is available

--- workoutizer/logger.py
+++ workoutizer/logger.py
@@ -1,38 +1,59 @@
-format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
+import os
+
+console_format = "%(asctime)s - %(levelname)s - %(name)s - %(message)s"
 minimal_format = "%(message)s"
 
 
-def _get_formatter_and_handler(use_minimal_format: bool = False):
+def _get_formatter_and_handler(path_to_log_dir: str, use_minimal_format: bool = False):
+    if not os.path.isdir(path_to_log_dir):
+        os.mkdir(path_to_log_dir)
     logging_dict = {
         "version": 1,
         "disable_existing_loggers": True,
         "formatters": {
             "colored": {
                 "()": "coloredlogs.ColoredFormatter",
-                "format": minimal_format if use_minimal_format else format,
+                "format": minimal_format if use_minimal_format else console_format,
                 "datefmt": "%m-%d %H:%M:%S",
-            }
+            },
+            "format_for_file": {
+                "format": "%(asctime)s :: %(levelname)s :: %(funcName)s in %(filename)s (l:%(lineno)d) :: %(message)s",
+                "datefmt": "%Y-%m-%d %H:%M:%S",
+            },
         },
         "handlers": {
             "console": {
                 "class": "logging.StreamHandler",
                 "formatter": "colored",
             },
+            "file": {
+                "level": "WARNING",
+                "class": "logging.handlers.RotatingFileHandler",
+                "formatter": "format_for_file",
+                "filename": os.path.join(path_to_log_dir, "wkz.log"),
+                "maxBytes": 20000,
+                "backupCount": 5,
+            },
         },
-        "loggers": {},
+        "loggers": {
+            "": {
+                "level": "DEBUG",
+                "handlers": ["console", "file"],
+            },
+        },
     }
     return logging_dict
 
 
-def get_logging_config(django_log_level: str, wkz_log_level: str):
-    logging_dict = _get_formatter_and_handler()
+def get_logging_config(django_log_level: str, wkz_log_level: str, path_to_log_dir: str):
+    logging_dict = _get_formatter_and_handler(path_to_log_dir=path_to_log_dir)
     logging_dict["loggers"] = {
         "django": {
-            "handlers": ["console"],
+            "handlers": ["console", "file"],
             "level": django_log_level,
         },
         "wizer": {
-            "handlers": ["console"],
+            "handlers": ["console", "file"],
             "level": wkz_log_level,
         },
     }
--- workoutizer/settings.py
+++ workoutizer/settings.py
@@ -117,4 +117,5 @@ trace_line_opacity = 0.9
 LOGGING = get_logging_config(
     django_log_level=os.getenv("DJANGO_LOG_LEVEL", "INFO"),
     wkz_log_level=os.getenv("WKZ_LOG_LEVEL", "INFO"),
+    path_to_log_dir=WORKOUTIZER_DIR,
 )
