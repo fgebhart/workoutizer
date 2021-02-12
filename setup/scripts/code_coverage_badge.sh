c7a7a928be7bac8ba2d60c259d03f41a1c1d6345
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-02-12 21:56:56 +0100

    add code coverage badge to readme, run convenience script to update

---
+++ setup/scripts/code_coverage_badge.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# install tool to create coverage badge from data created by pytest-cov located in .coverage folger
+pip install coverage-badge
+
+echo "Note: Run tests with coverage to update coverage data: pytest wizer/tests -n4 --cov=wizer"
+
+# create badge and save it to .github folder
+coverage-badge -o .github/badges/coverage.svg -f
