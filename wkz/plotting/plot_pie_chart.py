2b8244418b728fef9dd2318f9b41843a2af16ef6
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-04-07 07:44:15 +0200

    migrate dashboard to new design

--- wkz/plotting/plot_pie_chart.py
+++ wkz/plotting/plot_pie_chart.py
@@ -24,16 +24,16 @@ def plot_pie_chart(activities):
         if activity.sport.name in sport_distribution:
             sport_distribution[activity.sport.name] += 1
 
-    data = pd.Series(sport_distribution).reset_index(name="value").rename(columns={"index": "country"})
+    data = pd.Series(sport_distribution).reset_index(name="value").rename(columns={"index": "sport"})
     data["angle"] = data["value"] / data["value"].sum() * 2 * pi
     data["color"] = color_list
 
     p = figure(
-        plot_height=120,
+        plot_height=140,
         toolbar_location=None,
         sizing_mode="stretch_width",
         tools="hover",
-        tooltips="@country: @value",
+        tooltips="@sport: @value",
         x_range=(-0.5, 1.0),
     )
 
@@ -52,8 +52,6 @@ def plot_pie_chart(activities):
     p.axis.visible = False
     p.grid.grid_line_color = None
     p.outline_line_color = None
-    p.background_fill_color = "whitesmoke"
-    p.border_fill_color = "whitesmoke"
 
     script_pc, div_pc = components(p)
 
