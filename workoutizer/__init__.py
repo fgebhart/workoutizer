024dad4fcde3fe9bfb4d2a0e28cd5bbefa57ab33
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-07-05 20:15:18 +0200

    BLD: parse version from git tags and comply with PEP440

--- workoutizer/__init__.py
+++ workoutizer/__init__.py
@@ -1,1 +1,21 @@
-__version__ = "0.20.1"
+import os
+import subprocess
+from pathlib import Path
+
+
+def _get_version() -> str:
+    cwd = Path.cwd()
+    os.chdir(Path(__file__).parent.parent)
+    try:
+        cmd = ["python", "-W", "ignore", "setup.py", "-V"]
+        out = subprocess.check_output(cmd, stderr=subprocess.DEVNULL)
+        if not isinstance(out, str):
+            out = out.decode("utf-8")
+        version = out.strip()
+    except Exception:
+        version = "unknown"
+    os.chdir(cwd)
+    return version
+
+
+__version__ = _get_version()
