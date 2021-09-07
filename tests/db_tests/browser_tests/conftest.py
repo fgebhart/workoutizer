f3b27a9b38c5bf428a6cb0cf5ad3877e3bb76b00
Fabian Gebhart <fabian.gebhart@gmail.com>
2021-09-07 21:59:11 +0200

    TST: add test for bokeh button rendering different lap lines

--- tests/db_tests/browser_tests/conftest.py
+++ tests/db_tests/browser_tests/conftest.py
@@ -30,8 +30,16 @@ def take_screenshot():
     Helper function which is useful when debugging selenium browser tests.
     """
 
-    def _take(webdriver, name="screenshot"):
-        path = Path(django_settings.BASE_DIR) / "selenium_ide/debugging" / Path(f"{datetime.datetime.now()}_{name}.png")
+    def _take(webdriver, name: str = None, path: str = None):
+        if name is None:
+            name = f"{datetime.datetime.now()}_screenshot.png"
+        if not name.endswith(".png"):
+            name = f"{name}.png"
+        if path is None:
+            path = Path(django_settings.BASE_DIR) / "selenium_ide/debugging" / name
+        else:
+            path = Path(path) / name
+
         webdriver.save_screenshot(str(path))
 
     return _take
